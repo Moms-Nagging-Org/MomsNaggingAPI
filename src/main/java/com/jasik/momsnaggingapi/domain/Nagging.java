cf0c91e0447ab06b1e0e4351cfc598e723f76478
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-10 23:05:31 +0900

    feat : Nagging 도메인 클래스 작성

---
+++ src/main/java/com/jasik/momsnaggingapi/domain/Nagging.java
@@ -0,0 +1,38 @@
+package com.jasik.momsnaggingapi.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Nagging {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(columnDefinition = "TEXT")
+    private String custom;
+    @Column(columnDefinition = "TEXT")
+    private String level1;
+    @Column(columnDefinition = "TEXT")
+    private String level2;
+    @Column(columnDefinition = "TEXT")
+    private String level3;
+
+    @Builder
+    public Nagging(Long userId, String custom, String level1, String level2, String level3) {
+        this.userId = userId;
+        this.custom = custom;
+        this.level1 = level1;
+        this.level2 = level2;
+        this.level3 = level3;
+    }
+
+}
