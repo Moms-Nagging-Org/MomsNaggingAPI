3235df2dc9babf628d465c662ac861c09d0ff62f
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-10 22:56:52 +0900

    feat : Diary 도메인 클래스 작성

---
+++ src/main/java/com/jasik/momsnaggingapi/domain/Diary.java
@@ -0,0 +1,33 @@
+package com.jasik.momsnaggingapi.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+
+    private String title;
+    private String context;
+    private LocalDateTime diaryDate;
+
+    @Builder
+    public Diary(Long userId, String title, String context, LocalDateTime diaryDate) {
+        this.userId = userId;
+        this.title = title;
+        this.context = context;
+        this.diaryDate = diaryDate;
+    }
+
+}
