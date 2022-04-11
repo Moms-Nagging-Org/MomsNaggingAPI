5379385f638b1d55477a8c0c7a471c0069d405a0
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-11 19:16:58 +0900

    refactor : Diary 도메인 패키키 변경 및 3tier 기본 클래스 생성

---
+++ src/main/java/com/jasik/momsnaggingapi/domain/diary/service/DiaryService.java
@@ -0,0 +1,13 @@
+package com.jasik.momsnaggingapi.domain.diary.service;
+
+import com.jasik.momsnaggingapi.domain.diary.repository.DiaryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class DiaryService {
+    private final DiaryRepository diaryRepository;
+}
