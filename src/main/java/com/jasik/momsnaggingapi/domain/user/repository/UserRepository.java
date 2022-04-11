bad12ca6435608b0385149151a99102ba2fba376
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-11 19:14:40 +0900

    refactor : User 도메인 패키키 변경 및 3tier 기본 클래스 생성

---
+++ src/main/java/com/jasik/momsnaggingapi/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jasik.momsnaggingapi.domain.user.repository;
+
+import com.jasik.momsnaggingapi.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
