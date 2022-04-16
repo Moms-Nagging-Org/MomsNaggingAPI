0d0b5053cac1f8f86c38cb8b79908734ec8d2c89
sejong <sejong055@gmail.com>
2022-04-17 01:45:35 +0900

    etc  : mapper Bean 추가
    - ModelMapper : DAO <-> DTO 변환에 사용
    - ObjectMapper : Json <-> DTO 변환에 사용

---
+++ src/main/java/com/jasik/momsnaggingapi/infra/common/JsonPatchHttpMessageConverter.java
@@ -0,0 +1,42 @@
+package com.jasik.momsnaggingapi.infra.common;
+
+import javax.json.Json;
+import javax.json.JsonPatch;
+import javax.json.JsonReader;
+import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JsonPatchHttpMessageConverter extends AbstractHttpMessageConverter<JsonPatch> {
+
+    public JsonPatchHttpMessageConverter() {
+        super(MediaType.valueOf("application/json-patch+json"));
+    }
+
+    @Override
+    protected JsonPatch readInternal(Class<? extends JsonPatch> clazz, HttpInputMessage inputMessage) throws HttpMessageNotReadableException {
+
+        try (JsonReader reader = Json.createReader(inputMessage.getBody())) {
+            return Json.createPatch(reader.readArray());
+        } catch (Exception e) {
+            throw new HttpMessageNotReadableException(e.getMessage(), inputMessage);
+        }
+    }
+
+    @Override
+    protected void writeInternal(JsonPatch jsonPatch, HttpOutputMessage outputMessage) throws HttpMessageNotWritableException {
+        throw new NotImplementedException("The write Json patch is not implemented");
+    }
+
+    @Override
+    protected boolean supports(Class<?> clazz) {
+        return JsonPatch.class.isAssignableFrom(clazz);
+    }
+
+}
