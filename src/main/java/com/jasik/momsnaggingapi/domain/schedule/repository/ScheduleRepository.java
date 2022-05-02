e0378fde55ec58583768aef45924ee13d639589a
sejong <sejong055@gmail.com>
2022-05-03 02:01:03 +0900

    feat : schedule update all 쿼리 추가
    ########제목########
    <타입> : <제목> 의 형식으로 제목 작성
    제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    예) feat : 로그인 기능 추가
    
    #######본문#########
    작업 내용 간략히 작성
    여러 줄의 메시지를 작성할 땐 "-"로 구분
    예) 이메일, 패스워드 입력으로 로그인
    
    #######꼬릿말#########
    현재 커밋과 관련된 이슈 번호 작성, 여러개 가능
    예) close #7
    
    ########타입########
    feat : 새로운 기능 추가
    fix : 버그 수정
    docs : 문서 수정
    test : 테스트 코드 추가
    refact : 코드 리팩토링
    style : 코드 의미에 영향을 주지 않는 변경사항
    chore : 빌드 부분 혹은 패키지 매니저 수정사항
    etc : 기타 사항

--- src/main/java/com/jasik/momsnaggingapi/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/jasik/momsnaggingapi/domain/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,9 @@
 package com.jasik.momsnaggingapi.domain.schedule.repository;
 
-import com.jasik.momsnaggingapi.domain.schedule.Category;
 import com.jasik.momsnaggingapi.domain.schedule.Schedule;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -9,10 +11,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
@@ -23,11 +21,16 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Transactional
     @Modifying(clearAutomatically = true)
     @Query("delete from Schedule where id >= :scheduleId and userId = :userId and originalId = :originalId")
-    void deleteWithIdAfter(
-            @Param("scheduleId") Long scheduleId,
-            @Param("userId") Long userId,
-            @Param("originalId") Long originalId
-    );
+    void deleteWithIdAfter(@Param("scheduleId") Long scheduleId, @Param("userId") Long userId,
+        @Param("originalId") Long originalId);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Schedule set scheduleName = :scheduleName, scheduleTime = :scheduleTime, alarmTime = :alarmTime where id > :scheduleId and userId = :userId and originalId = :originalId")
+    void updateWithIdAfter(@Param("scheduleName") String scheduleName,
+        @Param("scheduleTime") String scheduleTime, @Param("alarmTime") LocalTime alarmTime,
+        @Param("scheduleId") Long scheduleId, @Param("userId") Long userId,
+        @Param("originalId") Long originalId);
 
     List<Schedule> findAllByCategoryId(Long categoryId);
 
