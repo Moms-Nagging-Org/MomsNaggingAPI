3a022b9c8de9bade1e2904e07d488792d25c9b7d
sejong <sejong055@gmail.com>
2022-05-14 23:34:59 +0900

    docs: 엔드포인트 설명 추가

--- src/main/java/com/jasik/momsnaggingapi/domain/grade/controller/GradeController.java
+++ src/main/java/com/jasik/momsnaggingapi/domain/grade/controller/GradeController.java
@@ -25,12 +25,21 @@ public class GradeController {
     private final GradeService gradeService;
 
     @GetMapping("/lastWeek")
-    @Operation(summary = "직전 주의 주간평가 조회", description = "페이지 : 홈\n\n직전 주차의 주간평가를 조회합니다. \n\n새롭게 달성한 상장 달성도를 함께 반환합니다.")
+    @Operation(summary = "직전 주의 주간평가 조회", description = ""
+        + "<페이지>\n\n"
+        + "홈\n\n"
+        + "<설명>\n\n"
+        + "직전 주차의 주간평가를 조회합니다. \n\n"
+        + "새롭게 달성한 상장 달성도를 함께 반환합니다.")
     public ResponseEntity<Grade.GradeResponse> getGradeOfLastWeek() {
         return ResponseEntity.ok().body(gradeService.getGradeOfLastWeek());
     }
     @GetMapping("/monthly")
-    @Operation(summary = "월간 주간평가 조회", description = "페이지 : 성적표 -> 통계 -> 특정 월 선택\n\n해당 년도, 월의 모든 주간평가를 조회합니다.")
+    @Operation(summary = "월간 주간평가 조회", description = ""
+        + "<페이지>\n\n"
+        + "성적표 -> 통계 -> 특정 월 선택\n\n"
+        + "<설명>\n\n"
+        + "해당 년도, 월의 모든 주간평가를 조회합니다.")
     public ResponseEntity<List<GradesOfMonthResponse>> getWeeklyGradesOfMonth(
         @Schema(example = "2022", required = true) @Parameter(name = "retrieveYear", description = "조회할 년도", in = ParameterIn.QUERY) @RequestParam int retrieveYear,
         @Schema(example = "5", required = true) @Parameter(name = "retrieveMonth", description = "조회할 월", in = ParameterIn.QUERY) @RequestParam int retrieveMonth) {
@@ -39,13 +48,21 @@ public class GradeController {
     }
     // TODO: Get-Me 엔드포인트에 포함시킬 데이터
     @GetMapping("/awards")
-    @Operation(summary = "상장 등급 조회", description = "페이지 : 성적표 -> 상장\n\n사용자가 달성한 상장 등급을 조회합니다.")
+    @Operation(summary = "상장 등급 조회", description = ""
+        + "<페이지>\n\n"
+        + "성적표 -> 상장\n\n"
+        + "<설명>\n\n"
+        + "사용자가 달성한 상장 등급을 조회합니다.")
     public ResponseEntity<Grade.AwardResponse> getAwards() {
         return ResponseEntity.ok().body(gradeService.getAwards());
     }
 
     @GetMapping("/calendar")
-    @Operation(summary = "월간 달력 성적표 조회", description = "페이지 : 성적표 -> 달력 -> 특정 월 선택\n\n해당 년도, 월의 모든 일별 달성도를 조회합니다.")
+    @Operation(summary = "월간 달력 성적표 조회", description = ""
+        + "<페이지>\n\n"
+        + "성적표 -> 달력 -> 특정 월 선택\n\n"
+        + "<설명>\n\n"
+        + "해당 년도, 월의 모든 일별 달성도를 조회합니다.")
     public ResponseEntity<List<Grade.Performance>> getDailyGradesOfMonth(
         @Schema(example = "2022", required = true) @Parameter(name = "retrieveYear", description = "조회할 년도", in = ParameterIn.QUERY) @RequestParam int retrieveYear,
         @Schema(example = "05", required = true) @Parameter(name = "retrieveMonth", description = "조회할 월", in = ParameterIn.QUERY) @RequestParam int retrieveMonth) {
@@ -53,7 +70,11 @@ public class GradeController {
     }
 
     @GetMapping("/statistics")
-    @Operation(summary = "성적표 통계 조회", description = "페이지 : 성적표 -> 통계\n\n통계 수치를 조회합니다.")
+    @Operation(summary = "성적표 통계 조회", description = ""
+        + "<페이지>\n\n"
+        + "성적표 -> 통계\n\n"
+        + "<설명>\n\n"
+        + "통계 수치를 조회합니다.")
     public ResponseEntity<Grade.StatisticsResponse> getStatistics(){
         return ResponseEntity.ok().body(gradeService.getStatistics());
     }
