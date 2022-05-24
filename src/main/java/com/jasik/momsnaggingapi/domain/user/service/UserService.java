c022969096a086b35d2a66e5240130581ff72ef8
Loy-Yun <yoonsoji@gmail.com>
2022-05-24 23:24:16 +0900

    fix: ModelMapper null issue

--- src/main/java/com/jasik/momsnaggingapi/domain/user/service/UserService.java
+++ src/main/java/com/jasik/momsnaggingapi/domain/user/service/UserService.java
@@ -6,8 +6,10 @@ import com.jasik.momsnaggingapi.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -21,17 +23,19 @@ public class UserService {
     private final ModelMapper modelMapper;
 
     public User.UserResponse findUser(String token) {
-        User user = userRepository.findById(authservice.getId(token)).orElseThrow(null);
-        log.info(user.getNickName());
-        return modelMapper.map(userRepository.findById(authservice.getId(token)), User.UserResponse.class);
+        User user = userRepository.findById(authservice.getId(token))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."));
+        return modelMapper.map(user, User.UserResponse.class);
     }
 
     public User.UserResponse editUser(String token) {
+        User user = userRepository.findById(authservice.getId(token))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."));
         return modelMapper.map(userRepository.findById(authservice.getId(token)), User.UserResponse.class);
     }
 
-    public Optional<User> findUserByPersonalId(String id) {
-        return userRepository.findByPersonalId(id);
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
     }
 
     public Long findUserIdByPersonalId(String personalId) {
