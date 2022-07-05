5cf5331b266ab868f45d48c88b2986a83141d887
sejong <sejong055@gmail.com>
2022-07-05 23:40:08 +0900

    etc: appversion 도메인 삭제

--- src/main/java/com/jasik/momsnaggingapi/domain/appVersion/controller/AppVersionController.java
+++
@@ -1,7 +0,0 @@
-package com.jasik.momsnaggingapi.domain.appVersion.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AppVersionController {
-}
