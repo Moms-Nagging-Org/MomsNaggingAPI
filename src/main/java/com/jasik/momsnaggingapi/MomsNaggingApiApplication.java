f1cd87fdbb5517989ae4ee96b77cbbe6e80b74fc
sejong <sejong055@gmail.com>
2022-08-30 00:06:43 +0900

    bug: 리눅스 타임 존 서울로 변경

--- src/main/java/com/jasik/momsnaggingapi/MomsNaggingApiApplication.java
+++ src/main/java/com/jasik/momsnaggingapi/MomsNaggingApiApplication.java
@@ -1,5 +1,7 @@
 package com.jasik.momsnaggingapi;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,7 +13,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 public class MomsNaggingApiApplication {
-
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(MomsNaggingApiApplication.class, args);
     }
