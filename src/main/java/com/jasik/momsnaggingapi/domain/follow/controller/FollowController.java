f9bb26ad89ff6be97db6864db71d22a9aea34320
Loy-Yun <yoonsoji@gmail.com>
2023-01-02 23:03:56 +0900

    docs: follow controller url [friends -> follows] 변경

--- src/main/java/com/jasik/momsnaggingapi/domain/follow/controller/FollowController.java
+++ src/main/java/com/jasik/momsnaggingapi/domain/follow/controller/FollowController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/friends")
+@RequestMapping("/api/v1/follows")
 @RequiredArgsConstructor
 @Tag(name = "Follow API !!!", description = "식구 팔로우/팔로워 API")
 public class FollowController {
